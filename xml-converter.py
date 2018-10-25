795a1722d70f6d038889157d495351a5b07e6b4f
César J. Fois <fois2010@gmail.com>
2018-10-25 13:48:17 -0300

    primeiro commit

---
+++ xml-converter.py
@@ -0,0 +1,47 @@
+import os
+import xml.etree.ElementTree as et
+
+
+# variavel = retorna o diretorio como string
+base_path = os.path.dirname(os.path.realpath(__file__))
+
+# join base_path with data folder.
+#variavel = caminho string + nome do arquivo
+
+xml_file = os.path.join(base_path, "read.xml")
+
+# variavel guarda o xml file dentro da memoria
+tree = et.parse(xml_file)
+
+root = tree.getroot()
+
+"""
+print(root)
+print(root.tag)
+print(root.attrib)
+print(root.tag, root.attrib)
+"""
+
+"""
+#listando in root
+for child in root:
+    print(child.tag ,child.attrib)
+"""
+"""
+#listando todos os elementos
+for child in root:
+    print()
+    for element in child:
+        print(element.tag, ":", element.text)
+"""
+"""
+#imprimindo um elemento
+print(root[1][1].text)
+"""
+
+"""
+#Buscando um elemento de interesse
+for neighbor in root.iter('product'):
+      print(neighbor.attrib)
+
+"""
